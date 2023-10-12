06bc6914e7e20fd6d0440cbafa0ccfbb3b093610
prostodre <deer7221@gmail.com>
2023-10-12 19:39:21 +0200

    Sass

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,12 +1,17 @@
 import "./.AboutUs.scss";
 
-function AboutUs (){
-    return(
-        <div className="aboutUs__section" id="aboutUs">
-       <p className="aboutUs__text">PLANT - a living thing that grows in earth, but from now on it is also the series of electronic events based in Kraków. The main goal of our community is to take roots on Krakow’s map and PLANT a fresh, groovy vibes among electronic music lovers. Every member of the community will blossom on the scene like a flower🌺.</p> 
+function AboutUs() {
+  return (
+    <div className="aboutUs__section" id="aboutUs">
+      <p className="aboutUs__text">
+        PLANT - a living thing that grows in earth, but from now on it is also
+        the series of electronic events based in Kraków. The main goal of our
+        community is to take roots on Krakow’s map and PLANT a fresh, groovy
+        vibes among electronic music lovers. Every member of the community will
+        blossom on the scene like a flower🌺.
+      </p>
     </div>
-    )
-    
+  );
 }
 
-export default  AboutUs;
+export default AboutUs;
