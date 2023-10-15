32e384329c40b8f7e64b14ef90678f329da6622f
prostodre <deer7221@gmail.com>
2023-10-15 19:00:51 +0200

    onClick

--- src/Events.jsx
+++ src/Events.jsx
@@ -1,21 +1,31 @@
-
 import "./.Events.scss";
 
 function Events() {
+  const handleClick2 = () => {
+    window.open(
+      "https://ra.co/events/1727306", "_blank"); 
+  };
+
+  const handleClick1 = () => {
+    window.open(
+      "https://ra.co/events/1727344", "_blank"); 
+  };
+
+  }
+
   return (
     <section className="events" id="events">
       <div className="events__heading">
         <h2>PREVIOUS EVENTS</h2>
       </div>
       <div className="events__photos">
-        <div className="events__image1">
-          {/* <img src="src/assets-photos/Plant23.jpg"/> */}
-        </div>
-        
-        <p>Our events usually take place in an underground spaces or bars such as Sekta Selekta, Święta Krowa. The main genres we focus on are electro, breaks, techno and trance.</p>
-        <div className="events__image2">
-          {/* <img src="src/assets-photos/24.06 plant poster1 (1).png"/> */}
-        </div>
+        <div onClick={handleClick1}className="events__image1"></div>
+        <p>
+          Our events usually take place in an underground spaces or bars such as
+          Sekta Selekta, Święta Krowa. The main genres we focus on are electro,
+          breaks, techno and trance.
+        </p>
+        <div onClick = {handleClick2} className="events__image2"></div>
       </div>
     </section>
   );
