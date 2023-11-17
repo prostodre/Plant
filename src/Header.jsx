d2be5b6389f94fe32fbce5305161f9843ae082e6
prostodre <deer7221@gmail.com>
2023-11-17 19:01:48 +0100

    responsive header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,30 @@
+import "./.Header.scss";
 
-import "./.Header.scss"
-
-function Header (){
-    return (
-        <div className="header">
-            <div className="header__logo">
-                <a href="#"><p className="logo">PLANT</p></a>
-            </div>
-            <div className="header__nav">
-              <nav>
-                <ul className='nav__list'>
-                  <li className='about'><a href="#aboutUs">ABOUT US</a></li>
-                  <li className='events'><a href="#events">EVENTS</a></li>
-                  <li className='contact'><a href="#contact">CONTACT</a></li>
-                </ul>
-            
-              </nav>
-            </div>
-        </div>
-    )
-    }
-    export default Header
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <a href="#">
+          <p className="logo">PLANT</p>
+        </a>
+      </div>
+      <div className="header__nav">
+        <nav>
+          <ul className="nav__list">
+            <li className="nav__element">
+              <a className="nav__link" href="#aboutUs">ABOUT US</a>
+            </li>
+            <li className="nav__element">
+              <a className="nav__link" href="#events">EVENTS</a>
+            </li>
+            <li className="nav__element">
+              <a className="nav__link" href="#contact">CONTACT</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      
+    </div>
+  );
+}
+export default Header;
